630dbb9e959113ebb82a41375900890ba63aaaa9
sarahcoll49 <scoll49@lsu.edu>
2023-04-11 10:14:37 -0500

    process one

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// JS scripts placed here
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function() {
+    header.classList.toggle (("sticky", window.scrollY > 0))
+})
